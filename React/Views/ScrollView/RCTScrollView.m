128b515320be3cfe6f6df71a5c55024e3760b250
Fredrik Palm <fredrik@thefarm.se>
2020-02-18 11:23:27 +0100

    Use scrollView.refreshControl instead of addSubview
    Fix for 0.61.5 release

--- React/Views/ScrollView/RCTScrollView.m
+++ React/Views/ScrollView/RCTScrollView.m
@@ -354,7 +354,7 @@ RCT_NOT_IMPLEMENTED(- (instancetype)init)
     [_customRefreshControl removeFromSuperview];
   }
   _customRefreshControl = refreshControl;
-  [self addSubview:_customRefreshControl];
+  self.refreshControl = _customRefreshControl;
 }
 
 - (void)setPinchGestureEnabled:(BOOL)pinchGestureEnabled
